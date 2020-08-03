2764bbb9e6e5eeb8dd36732c7bb0f456bd91fb23
denskarlet <den.skarlet@gmail.com>
2020-08-03 15:07:54 -0400

    moved helpers into util

--- server/controllers/locationController.js
+++ server/controllers/locationController.js
@@ -1,81 +1,41 @@
 const fetch = require('node-fetch');
-const superagent = require('superagent');
 const jwt = require('jsonwebtoken');
 const db = require('../db');
 const { client_id, client_secret, redirect_uri, mySecret, weatherKey } = require('../../secret');
+const {
+  fetchSpotifyApi,
+  fetchWeatherApi,
+  fetchCountryApi,
+  parseCountryResponse,
+  parseWeatherResponse,
+  parseSpotifyResponse,
+} = require('../util/locationUtil');
 
-const getWeatherUrl = (lat, lon) => {
-  return `https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${lat}&lon=${lon}&appid=${weatherKey}`;
-};
-const getCountryUrl = (name) => {
-  return `https://restcountries.eu/rest/v2/name/${name}`;
-};
-const getSpotifyUrl = (code) => {
-  return `https://api.spotify.com/v1/browse/categories/toplists/playlists?country=${code}`;
-};
 const locationController = {};
 
 locationController.getLocationData = async (req, res, next) => {
-  const { lat, lon, country, city } = req.body;
-  const responses = await Promise.all([
-    fetch(getWeatherUrl(lat, lon)),
-    fetch(getCountryUrl(country)),
+  const { lat, lon, country, city } = req.query;
+  const [weatherData, [countryData]] = await Promise.all([
+    fetchWeatherApi(lat, lon, weatherKey),
+    fetchCountryApi(country),
   ]);
-  const result = await Promise.all(responses.map((response) => response.json()));
-  const { alpha2Code } = result[1][0];
-  const encrypted = await jwt.verify(req.cookies.token, mySecret);
-  const { access_token } = encrypted;
-  const spotifyResponse = await fetch(getSpotifyUrl(alpha2Code), {
-    headers: { Authorization: `Bearer ${access_token}` },
-  });
-  const spotifyData = await spotifyResponse.json();
+  const { alpha2Code } = countryData;
+  const { access_token } = await jwt.verify(req.cookies.token, mySecret);
+  const spotifyData = await fetchSpotifyApi(alpha2Code, access_token);
   res.locals.locationData = {
-    weatherData: result[0],
-    countryData: result[1],
+    weatherData,
+    countryData,
     spotifyData,
   };
   return next();
 };
+
 locationController.parseData = (req, res, next) => {
   const { spotifyData, weatherData, countryData } = res.locals.locationData;
-  const { country } = req.body;
-  const playlist = spotifyData.playlists.items.find((item) => item.name === `${country} Top 50`)
-    .tracks.href;
-  const weatherInfo = {
-    weather: weatherData.weather[0].main,
-    temp: weatherData.main.temp,
-    feels_like: weatherData.main.feels_like,
-    temp_min: weatherData.main.temp_min,
-    temp_max: weatherData.main.temp_max,
-    humidity: weatherData.main.humidity,
-    windSpeed: weatherData.wind.speed,
-    sunrise: weatherData.sys.sunrise,
-    sunset: weatherData.sys.sunset,
-    timezone: weatherData.timezone,
-  };
-  const [countryObj] = countryData;
-  const {
-    name,
-    alpha2Code,
-    capital,
-    region,
-    area,
-    population,
-    languages,
-    flag,
-    borders,
-  } = countryObj;
-  const countryInfo = {
-    name,
-    alpha2Code,
-    capital,
-    region,
-    area,
-    population,
-    languages,
-    flag,
-    borders,
-  };
+  const { country } = req.query;
+  const playlist = parseSpotifyResponse(spotifyData, country);
+  const weatherInfo = parseWeatherResponse(weatherData);
+  const countryInfo = parseCountryResponse(countryData);
   res.locals.locationData = {
     countryInfo,
     weatherInfo,
---
+++ server/util/locationUtil.js
@@ -0,0 +1,63 @@
+const fetch = require('node-fetch');
+const jwt = require('jsonwebtoken');
+
+exports.fetchSpotifyApi = (countryCode, access_token) => {
+  const url = `https://api.spotify.com/v1/browse/categories/toplists/playlists?country=${countryCode}`;
+  const reqOptions = {
+    headers: { Authorization: `Bearer ${access_token}` },
+  };
+  return fetch(url, reqOptions).then((res) => res.json());
+};
+exports.fetchWeatherApi = (lat, lon, key) => {
+  const url = `https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${lat}&lon=${lon}&appid=${key}`;
+  return fetch(url).then((res) => res.json());
+};
+exports.fetchCountryApi = (country) => {
+  const url = `https://restcountries.eu/rest/v2/name/${country}`;
+  return fetch(url).then((res) => res.json());
+};
+exports.parseSpotifyResponse = (obj, country) => {
+  return obj.playlists.items.find((item) => item.name === `${country} Top 50`).tracks.href;
+};
+exports.parseWeatherResponse = (obj) => {
+  return {
+    weather: obj.weather[0].main,
+    temp: obj.main.temp,
+    feelsLike: obj.main.feels_like,
+    tempMin: obj.main.temp_min,
+    tempMax: obj.main.temp_max,
+    humidity: obj.main.humidity,
+    windSpeed: obj.wind.speed,
+    sunrise: obj.sys.sunrise,
+    sunset: obj.sys.sunset,
+    timezone: obj.timezone,
+  };
+};
+exports.parseCountryResponse = (obj) => {
+  const {
+    name,
+    nativeName,
+    alpha2Code,
+    capital,
+    region,
+    area,
+    population,
+    languages,
+    flag,
+    borders,
+    regionalBlocs,
+  } = obj;
+  return {
+    regionalBlocs,
+    nativeName,
+    name,
+    alpha2Code,
+    capital,
+    region,
+    area,
+    population,
+    languages,
+    flag,
+    borders,
+  };
+};
