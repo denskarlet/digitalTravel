846255d74b4ae5667004251ba097f66ba2ab96e4
denskarlet <den.skarlet@gmail.com>
2020-08-19 13:21:29 -0400

    added actions to load favorites

--- client/actions/actions.js
+++ client/actions/actions.js
@@ -1,3 +1,6 @@
 export const LOADING = 'LOADING';
 export const RESPONSE_COMPLETE = 'RESPONSE_COMPLETE';
 export const ERROR = 'ERROR';
+export const LOADED_DATA = 'LOADED_DATA';
+export const ADD_FAVORITE = 'ADD_FAVORITE';
+export const REMOVE_FAVORITE = 'REMOVE_FAVORITE';
