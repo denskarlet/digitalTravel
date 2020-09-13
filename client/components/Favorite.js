bbf7646c0cc737980586e4f44118aaaa14df16d6
denskarlet <den.skarlet@gmail.com>
2020-09-13 12:41:57 -0400

    minor

--- client/components/Favorite.js
+++ client/components/Favorite.js
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import { removeFav } from '../actions';
 
 const Favorite = ({ data, dispatch, setQuery }) => {
   const { favorite_id } = data;
+  const handleRemove = useCallback(() => dispatch(removeFav(favorite_id)), [favorite_id, dispatch]);
   return (
     <>
       <pre>{JSON.stringify(data, null, 2)}</pre>
-      <button type="button" onClick={() => dispatch(removeFav(favorite_id))}>
+      <button type="button" onClick={handleRemove}>
         Remove
       </button>
       <button type="button" onClick={() => setQuery(data)}>
