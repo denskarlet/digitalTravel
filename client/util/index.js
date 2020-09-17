ae411511429bede577a64b4e22ebdd7d65aaba3d
denskarlet <den.skarlet@gmail.com>
2020-09-17 12:06:45 -0400

    added index to util

---
+++ client/util/index.js
@@ -0,0 +1,6 @@
+import useThunkReducer from './useThunkReducer';
+import ProtectedRoute from './ProtectedRoute';
+import fetchReducer from './fetchReducer';
+import useFetch from './useFetch';
+
+export { useThunkReducer, useFetch, fetchReducer, ProtectedRoute };
