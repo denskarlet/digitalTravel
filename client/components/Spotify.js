73d615e8b8fb4c335803963822498cd736e2449b
denskarlet <den.skarlet@gmail.com>
2020-09-19 12:08:07 -0400

    spotify button

--- client/components/Spotify.js
+++ client/components/Spotify.js
@@ -2,16 +2,20 @@ import React, { useState } from 'react';
 
 const Spotify = ({ data }) => {
   const [isLoading, setIsLoading] = useState(true);
+  const [minified, setMinified] = useState(true);
   if (!data) return <div>Sorry no spotify here</div>;
   const url = data.split('/');
   return (
     <div>
       {isLoading && <h3>Loading...</h3>}
+      <button type="button" onClick={() => setMinified(!minified)}>
+        {minified ? 'Expand' : 'Minify'}
+      </button>
       <iframe
         title="player"
         src={`https://open.spotify.com/embed/playlist/${url[url.length - 2]}`}
-        width="150"
-        height="170"
+        width="300"
+        height={minified ? '80' : '580'}
         frameBorder="0"
         onLoad={() => {
           setIsLoading(false);
