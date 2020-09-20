743b398731222fa475afad464575084f6d6a148c
denskarlet <den.skarlet@gmail.com>
2020-09-19 21:26:04 -0400

    adjusted router and removed loading animation of 404

--- client/UserProvider.js
+++ client/UserProvider.js
@@ -6,7 +6,8 @@ import UserContext from './contexts/UserContext';
 const UserProvider = ({ children }) => {
   const [isLogged, setIsLogged] = useState(!!Cookies.get('token'));
   const [userData, isLoading, error] = useFetch('/api/user');
-  if (isLoading) return <h1>Loading...</h1>;
+  console.log({ isLoading, isLogged });
+  if (isLoading && isLogged) return <h1>Loading...</h1>;
   return (
     <UserContext.Provider value={{ isLogged, userData, isLoading }}>
       {children}
--- client/components/App.js
+++ client/components/App.js
@@ -2,13 +2,16 @@ import React, { useContext, useEffect, useState } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './Home';
 import Login from './Login';
+import NotFound from './NotFound';
 import PrivateRoute from '../util/ProtectedRoute';
+import UserProvider from '../UserProvider';
 
 const App = () => {
   return (
     <Switch>
-      <PrivateRoute exact path="/" component={Home} redirect="/login" />
       <Route exact path="/login" component={Login} />
+      <PrivateRoute exact path="/" component={Home} redirect="/login" />
+      <NotFound />
     </Switch>
   );
 };
